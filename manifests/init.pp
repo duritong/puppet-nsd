e3195514db29442afc84eb0c5fc53ad0f235438a
mh <mh@immerda.ch>
2012-11-21 23:01:16 +0100

    add shorewall management

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,11 +5,15 @@
 #  * interface: do you want to restrict listening to a certain ipaddress?
 #  * manage_munin: do you want to manage munin plugin to monitor nsd?
 class nsd(
-  $interface = '',
-  $manage_munin = false
+  $interface        = '',
+  $manage_munin     = false,
+  $manage_shorewall = false
 ) {
   include nsd::base
   if $manage_munin {
     include nsd::munin
+  }
+  if $manage_shorewall {
+    include shorewall::rules::dns
   }
 }
