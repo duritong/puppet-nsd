926086d037915e365a23379e79ea7424de06fcd3
mh <mh@immerda.ch>
2012-11-21 23:27:45 +0100

    integrate nagios tests

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,10 +4,13 @@
 #
 #  * interface: do you want to restrict listening to a certain ipaddress?
 #  * manage_munin: do you want to manage munin plugin to monitor nsd?
+#  * manage_shorewall: open ports in shorewall?
+#  * nagios_test_domains: an array of domains that should tested to be resolved
 class nsd(
-  $interface        = '',
-  $manage_munin     = false,
-  $manage_shorewall = false
+  $interface           = '',
+  $manage_munin        = false,
+  $manage_shorewall    = false,
+  $nagios_test_domains = 'absent'
 ) {
   include nsd::base
   if $manage_munin {
@@ -15,5 +18,15 @@ class nsd(
   }
   if $manage_shorewall {
     include shorewall::rules::dns
+  }
+  if $nagios_test_domains != 'absent' {
+    nagios::service::dns{
+      $nagios_test_domains:
+        comment => 'nsd',
+        ip      => $nsd::interface ? { 
+          ''      => $::ipaddress,
+          default => $nsd::interface
+        }   
+    }   
   }
 }
